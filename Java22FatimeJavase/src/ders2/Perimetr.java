fc662dc8da5ffa5066246cb118f50c5456b8172f
Fatima Nuruzada <fatimenuruzade23@gmail.com>
2025-11-01 20:42:23 +0400

    Perimetr

--- Java22FatimeJavase/src/ders2/Perimetr.java
+++ Java22FatimeJavase/src/ders2/Perimetr.java
@@ -3,7 +3,10 @@ package ders2;
 public class Perimetr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int width = 12;
+		int length = 20;
+		int perimetr = 2*(width+length);
+		System.out.println(perimetr);
 
 	}
 
