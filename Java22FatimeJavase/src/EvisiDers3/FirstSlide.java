df67a406a5a00b75e0816a7d7c8302a150742974
Fatima <fatimenuruzade23@gmail.com>
2025-11-07 21:12:10 +0400

    Added main application class

---
+++ Java22FatimeJavase/src/EvisiDers3/FirstSlide.java
@@ -0,0 +1,34 @@
+package EvisiDers3;
+
+public class FirstSlide {
+public static void main(String[] args) {
+	short a = 69;
+	byte b =(byte)a;
+	System.out.println(b);
+	
+	short a2 = 589;
+	byte b2 =(byte)a2;
+	System.out.println(b2);
+	
+	long l = 458;
+	int i = (int)l;
+	System.out.println(i);
+	
+	long l2= 5252;
+	int i2 = (int)l2;
+	System.out.println(i2);
+	
+	double d = 5632.6;
+	float f = (float)d;
+	System.out.println(f);
+	
+	char c = 'D';
+	int i3 = (int)c;
+	System.out.println(i3);
+	
+	int i4 = 123;
+	char c2 = (char)i4;
+	System.out.println(c2);
+	
+}
+}
