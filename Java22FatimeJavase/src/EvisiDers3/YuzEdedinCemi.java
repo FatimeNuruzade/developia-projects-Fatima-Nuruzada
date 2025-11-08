90d14f37ed1060a4fb9484c54702c864dedbc865
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 11:42:29 +0400

    Yuz ededin cemi

---
+++ Java22FatimeJavase/src/EvisiDers3/YuzEdedinCemi.java
@@ -0,0 +1,12 @@
+package EvisiDers3;
+
+public class YuzEdedinCemi {
+public static void main(String[] args) {
+	int sum =0;
+	for(int a =0; a <=100;  a = a+1) {
+		System.out.println("1-100 araligi: "+ a);
+		sum +=a;
+	}
+	System.out.println(sum);
+}
+}
