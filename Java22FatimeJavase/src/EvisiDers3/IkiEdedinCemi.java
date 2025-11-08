9ab9744fd3257aa1768de66c8a12f78e7dcf886a
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 11:30:33 +0400

    Iki ededin cemi

---
+++ Java22FatimeJavase/src/EvisiDers3/IkiEdedinCemi.java
@@ -0,0 +1,12 @@
+package EvisiDers3;
+
+public class IkiEdedinCemi {
+public static void main(String[] args) {
+	int a = 5;
+	System.out.println(a);
+	int b = 10;
+	System.out.println(b);
+	int cem = a+b;
+	System.out.println("A ve B cemi: "+ cem);
+}
+}
