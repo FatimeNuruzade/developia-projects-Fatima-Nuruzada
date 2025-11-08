62de87134ea76e2e7e3e793b39fa231e8de3e071
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 11:32:01 +0400

    Iki ededin hasili

---
+++ Java22FatimeJavase/src/EvisiDers3/IkiEdedinHasili.java
@@ -0,0 +1,13 @@
+package EvisiDers3;
+
+public class IkiEdedinHasili {
+
+	public static void main(String[] args) {
+		int a = 8;
+		int b =11;
+		int hasil = a*b;
+		System.out.println("a ve b hasili: " +hasil);
+
+	}
+
+}
