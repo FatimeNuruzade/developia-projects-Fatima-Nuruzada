5e1f0370271f1901b3113fbcea56e20e1c334fe8
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 11:28:42 +0400

    AraliqCalismasi

---
+++ Java22FatimeJavase/src/EvisiDers3/AraliqCalismasi.java
@@ -0,0 +1,10 @@
+package EvisiDers3;
+
+public class AraliqCalismasi {
+public static void main(String[] args) {
+	
+	for (int a =0; a<=5; a++) {
+		System.out.println("1-5 araligi: " + a);
+	}
+}
+}
