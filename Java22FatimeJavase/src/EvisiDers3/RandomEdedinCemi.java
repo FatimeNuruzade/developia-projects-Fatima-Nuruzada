c4bebcc1ee49a1e8075a3d72bfde0fac7c4b2378
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 11:35:04 +0400

    RandomEdedinCemi

---
+++ Java22FatimeJavase/src/EvisiDers3/RandomEdedinCemi.java
@@ -0,0 +1,15 @@
+package EvisiDers3;
+
+import java.util.Scanner;
+
+public class RandomEdedinCemi {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	System.out.println(a);
+	int b = s.nextInt();
+	System.out.println(b);
+	int cem = a+b;
+	System.out.println(cem);
+}
+}
