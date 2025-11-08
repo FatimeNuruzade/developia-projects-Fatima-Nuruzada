3c35702e5dbbbe3182c25365808ce4b15b885bcf
Fatima <fatimenuruzade23@gmail.com>
2025-11-08 12:01:48 +0400

    Create IyirmiElliAraliqi.java

---
+++ Java22FatimeJavase/src/EvisiDers3/IyirmiElliAraliqi.java
@@ -0,0 +1,11 @@
+package EvisiDers3;
+
+public class IyirmiElliAraliqi {
+	public static void main(String[] args) {
+
+
+        for (int i = 50; i >= 20; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
