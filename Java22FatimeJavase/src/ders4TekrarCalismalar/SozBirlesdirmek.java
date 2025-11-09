47d2b3d64070e9748dc8b75110030e91e81b7d69
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 19:54:21 +0400

    sozleri birlesdirmek

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/SozBirlesdirmek.java
@@ -0,0 +1,15 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class SozBirlesdirmek {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	String a = s.nextLine();
+	System.out.println(a);
+	String b = s.nextLine();
+	System.out.println(b);
+	String birlesdir = a+b;
+	System.out.println(birlesdir);
+}
+}
