5e9fa45a1c5ac85d787cc523128646cab6aae624
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 19:45:10 +0400

    dovrler

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/Dovr1.java
@@ -0,0 +1,10 @@
+package ders4TekrarCalismalar;
+
+public class Dovr1 {
+public static void main(String[] args) {
+	int a =0;
+	for (int i = a; i <=10; i++) {
+		System.out.println(i);
+	}
+}
+}
---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/Dovr2.java
@@ -0,0 +1,18 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class Dovr2 {
+	  public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	      
+	        int n = input.nextInt();
+
+	        int sum = 0;
+	        for (int i = 1; i <= n; i++) {
+	            sum += i;
+	        }
+
+	        System.out.println(sum);
+	    }
+}
