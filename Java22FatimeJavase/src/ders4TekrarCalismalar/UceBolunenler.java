94e11f5c6aacaa3c77856e8c5852558f22e4c857
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 19:48:26 +0400

    Create UceBolunenler.java

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/UceBolunenler.java
@@ -0,0 +1,12 @@
+package ders4TekrarCalismalar;
+
+public class UceBolunenler {
+public static void main(String[] args) {
+	
+	for (int i = 0; i <=100; i++) {
+		if(i%3==0) {
+			System.out.println(i);
+		}
+	}
+}
+}
