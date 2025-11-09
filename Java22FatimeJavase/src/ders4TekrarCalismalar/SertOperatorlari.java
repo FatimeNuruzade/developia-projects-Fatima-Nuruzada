b32cce5771df999500727d404a0e8d34e9d4d5a8
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 19:04:41 +0400

    SertOperatoru

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/SertOperatorlari.java
@@ -0,0 +1,18 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class SertOperatorlari {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();{
+
+		if(a<0) {
+			System.out.println("Eded Menfidir");
+		}
+		else {
+			System.out.println("eded musbetdir");
+		}
+		}
+		}
+}
