ac11951d8af657fd2e08721482352a31cf780004
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 18:45:10 +0400

    ededi orta

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/EdediOrta.java
@@ -0,0 +1,13 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class EdediOrta {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int b = s.nextInt();
+	int c = (a+b)/2;
+	System.out.println(c);
+}
+}
