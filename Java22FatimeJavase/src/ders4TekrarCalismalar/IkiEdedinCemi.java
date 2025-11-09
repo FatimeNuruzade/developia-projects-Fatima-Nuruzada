b3ebcad202feb4ab62fa6ba347ea05ff581a333b
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 18:43:09 +0400

    Iki ededin cemi

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/IkiEdedinCemi.java
@@ -0,0 +1,17 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class IkiEdedinCemi {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	System.out.println("Ilk eded: "+a);
+	int b = s.nextInt();
+	System.out.println("Ikinci eded: "+b);
+	int c= a+b;
+	System.out.println("Cem: "+c);
+	
+	
+}
+}
