d79a2bf18ac0dfe21f3f1cc0740370bfcdd375fb
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 23:04:05 +0400

    Verilmis ededlerin araliglari

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/VerilmisEdedAraligi.java
@@ -0,0 +1,10 @@
+package ders4TekrarCalismalar;
+
+public class VerilmisEdedAraligi {
+public static void main(String[] args) {
+	int a =20;
+	for (int i = a; i <=50; i++) {
+		System.out.println(i);
+	}
+}
+}
