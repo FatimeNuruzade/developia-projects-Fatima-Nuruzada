dfd9207b794b7cb31136e8fb3a5eb73015745dea
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 19:51:33 +0400

    uzunluq

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/Uzunluq.java
@@ -0,0 +1,11 @@
+package ders4TekrarCalismalar;
+
+import java.util.Scanner;
+
+public class Uzunluq {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	String a = s.next();
+	System.out.println(a.length());
+}
+}
