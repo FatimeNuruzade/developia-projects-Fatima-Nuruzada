78bf8a918e5991dbcd2bab1ce6cf21e67891d4eb
Fatima <fatimenuruzade23@gmail.com>
2025-11-09 23:06:06 +0400

    Araliqdan cixan tek ededler

---
+++ Java22FatimeJavase/src/ders4TekrarCalismalar/AraliqdanTekEdedler.java
@@ -0,0 +1,12 @@
+package ders4TekrarCalismalar;
+
+public class AraliqdanTekEdedler {
+public static void main(String[] args) {
+	int a =15;
+	for (int i = a; i <=40; i++) {
+		if(i%3==0) {
+			System.out.println(i);
+		}
+	}
+}
+}
