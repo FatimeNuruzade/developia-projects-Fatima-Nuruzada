5d84623f34f8599cb8ec9cdbcbc2a892d3f09c5e
Fatima <fatimenuruzade23@gmail.com>
2025-11-14 19:38:47 +0400

    Create Massiv3.java

---
+++ Java22FatimeJavase/src/ders5/Massiv3.java
@@ -0,0 +1,13 @@
+package ders5;
+
+public class Massiv3 {
+public static void main(String[] args) {
+	int numbers[]= {1,2,3,4,5};
+	int tekEded=0;
+	for (int i = 0; i < numbers.length; i++) {
+		if(numbers%==numbers) {
+			
+		}
+	}
+}
+}
