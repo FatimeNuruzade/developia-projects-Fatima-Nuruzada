114cb324e360827dee97857251fd91b8616ea794
Fatima <fatimenuruzade23@gmail.com>
2025-11-14 19:57:59 +0400

    Massiv5EvTap

---
+++ Java22FatimeJavase/src/ders5EvTapsirigi/Massiv5.java
@@ -0,0 +1,15 @@
+package ders5EvTapsirigi;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Massiv5 {
+public static void main(String[] args) {
+	int[] numbers = {7,3,6,0,12,32};
+
+    Arrays.sort(numbers);
+    for (int num : numbers) {
+        System.out.print(num + " ");
+    }
+}
+}
