2e16902588163683af62402bfd22bc5f93e039de
Fatima <fatimenuruzade23@gmail.com>
2025-11-17 22:00:41 +0400

    Ders 6 Araliq tapsirigi

---
+++ Java22FatimeJavase/src/ders6EvTapsirigi/Main3.java
@@ -0,0 +1,10 @@
+package ders6EvTapsirigi;
+
+public class Main3 {
+public static void main(String[] args) {
+	MethodsHome a = new MethodsHome();
+	a.printNumber(2, 6);
+	a.printNumber(3, 12);
+	a.printNumber(30, 40);
+}
+}
---
+++ Java22FatimeJavase/src/ders6EvTapsirigi/MethodsHome.java
@@ -0,0 +1,11 @@
+package ders6EvTapsirigi;
+
+public class MethodsHome {
+void printNumber(int begin,int end) {
+	System.out.println("araliq: "+begin+"-"+end);
+	for (int i = begin; i <=end; i++) {
+		System.out.println(i + " ");
+	}
+	System.out.println();
+}
+}
