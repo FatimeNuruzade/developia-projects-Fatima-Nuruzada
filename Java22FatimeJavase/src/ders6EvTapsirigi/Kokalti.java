d9fdf0721f512eb22a6341a30747cdc1d4cdb59a
Fatima <fatimenuruzade23@gmail.com>
2025-11-17 22:15:23 +0400

    Kokalti

---
+++ Java22FatimeJavase/src/ders6EvTapsirigi/Kokalti.java
@@ -0,0 +1,10 @@
+package ders6EvTapsirigi;
+
+public class Kokalti {
+public static void main(String[] args) {
+	double a =64;
+	double result=  Math.sqrt(a);
+	System.out.println(a);
+	System.out.println(result);
+}
+}
