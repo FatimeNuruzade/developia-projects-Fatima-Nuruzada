99681c912703378617539d6812fa3f23bb15f3a3
Fatima <fatimenuruzade23@gmail.com>
2025-11-18 20:03:26 +0400

    ders 6 quvvet ustunu tapmaq

---
+++ Java22FatimeJavase/src/ders6EvTapsirigi/QuvvetUstu.java
@@ -0,0 +1,9 @@
+package ders6EvTapsirigi;
+
+public class QuvvetUstu {
+public static void main(String[] args) {
+	double a = 4;
+	double result = Math.pow(2, a);
+	System.out.println(result);
+}
+}
