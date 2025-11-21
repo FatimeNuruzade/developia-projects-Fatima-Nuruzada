d35d61c573447978049a1fce5a2a7f96e561be02
Fatima <fatimenuruzade23@gmail.com>
2025-11-21 20:35:20 +0400

    Siralama ders 7 ev isi

---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/Siralama.java
@@ -0,0 +1,12 @@
+package Ders7EvTapsirigi;
+
+import java.util.Arrays;
+
+public class Siralama {
+public static void main(String[] args) {
+	String[] names= {"Aysel", "Rauf", "Mahir"};
+	Arrays.sort(names);
+	for(String name : names) {
+	System.out.println(name);
+}}
+}
