54cd95150aa4fe261368254a2602a90ce6ae869c
Fatima <fatimenuruzade23@gmail.com>
2025-11-21 20:39:28 +0400

    Sinif metod ders 7 ev isi

---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/Car.java
@@ -0,0 +1,10 @@
+package Ders7EvTapsirigi;
+
+public class Car {
+String brand;
+int year;
+void printInfo() {
+	System.out.println(brand);
+	System.out.println(year);
+}
+}
---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/Main1.java
@@ -0,0 +1,11 @@
+package Ders7EvTapsirigi;
+
+public class Main1 {
+public static void main(String[] args) {
+	Car car = new Car();
+	car.brand="BMW";
+	car.year=2021;
+	car.printInfo();
+	
+}
+}
