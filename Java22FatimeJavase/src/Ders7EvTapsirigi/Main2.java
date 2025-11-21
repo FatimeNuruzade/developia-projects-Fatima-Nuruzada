c620d44f6eb5c7cf1368101f68f3fad1c52ad571
Fatima <fatimenuruzade23@gmail.com>
2025-11-21 20:42:17 +0400

    main 2 ders 7 ev isi

---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/Main2.java
@@ -0,0 +1,12 @@
+package Ders7EvTapsirigi;
+
+public class Main2 {
+	static int cem(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		int result = cem(2,3);
+		System.out.println(result);
+	}
+
+}
