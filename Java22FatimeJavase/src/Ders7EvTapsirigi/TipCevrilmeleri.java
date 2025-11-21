1d645c4e15ac88b8b334d47b9b708ffca3a71f2d
Fatima <fatimenuruzade23@gmail.com>
2025-11-21 20:18:13 +0400

    Tip cevirmeleri ders 7 ev isi

---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/TipCevrilmeleri.java
@@ -0,0 +1,12 @@
+package Ders7EvTapsirigi;
+
+public class TipCevrilmeleri {
+public static void main(String[] args) {
+	  int a =25; 
+	double d =a;
+ System.out.println(d);
+ double d2 =7.9;
+ int a2= (int) d2;
+ System.out.println(a2);
+}
+}
