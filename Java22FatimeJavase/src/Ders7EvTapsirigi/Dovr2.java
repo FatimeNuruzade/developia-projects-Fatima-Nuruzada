9726bd57e34e423ddbcdc38967f8d65a5be8ad32
Fatima <fatimenuruzade23@gmail.com>
2025-11-21 20:30:43 +0400

    While dovru ders 7 ev isi

---
+++ Java22FatimeJavase/src/Ders7EvTapsirigi/Dovr2.java
@@ -0,0 +1,13 @@
+package Ders7EvTapsirigi;
+
+public class Dovr2 {
+public static void main(String[] args) {
+	int i =1;
+	int cem =0;
+	while(i<=10) {
+		cem += i;
+		i++;
+	}
+	System.out.println(cem);
+}
+}
