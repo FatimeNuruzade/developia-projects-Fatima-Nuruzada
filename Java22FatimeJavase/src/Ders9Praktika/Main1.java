b6f5d8a2f661d30d69380bd9bd7ec82bc7496261
Fatima <fatimenuruzade23@gmail.com>
2025-11-26 20:39:51 +0400

    duzbucaqli2

--- Java22FatimeJavase/src/Ders9Praktika/Main1.java
+++ Java22FatimeJavase/src/Ders9Praktika/Main1.java
@@ -3,8 +3,9 @@ package Ders9Praktika;
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		 Rectangle r = new Rectangle(3,8);
+	        double p = r.calculatePerimeter();
+	        System.out.println(p);
+	    }
 
 }
--- Java22FatimeJavase/src/Ders9Praktika/Rectangle.java
+++ Java22FatimeJavase/src/Ders9Praktika/Rectangle.java
@@ -1,5 +1,14 @@
 package Ders9Praktika;
 
 public class Rectangle {
+	double width;
+    double length;
 
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+    public double calculatePerimeter() {
+        return 2 * (width + length);
+    }
 }
