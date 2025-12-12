d602853e838b0f1eeb85132996a9afc9b28adbf0
Fatima <fatimenuruzade23@gmail.com>
2025-12-12 20:48:34 +0400

    ders 12 tapsirig 1

---
+++ Java22FatimeJavase/src/ders12/Animal.java
@@ -0,0 +1,7 @@
+package ders12;
+
+public class Animal {
+void eat() {
+	System.out.println("Dog is eating");
+}
+}
---
+++ Java22FatimeJavase/src/ders12/Main1.java
@@ -0,0 +1,10 @@
+package ders12;
+
+public class Main1 {
+public static void main(String[] args) {
+	Animal a = new Animal();
+	a.eat();
+	Dog d = new Dog();
+	d.eat();
+}
+}
