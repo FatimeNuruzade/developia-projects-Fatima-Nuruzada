c741026dec92870d0f1bea42068016aec8b6876e
Fatima <fatimenuruzade23@gmail.com>
2026-02-03 21:05:22 +0400

    spring yeni proyekt

---
+++ new-spring/src/main/java/com/example/demo/SpringNewPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewPracticeApplication.class, args);
+	}
+
+}
